66076190d5c81a37810ad16b928ff7d4b8562b93
Xavi Grimau <xgrimau@gmail.com>
2021-06-08 21:30:49 +0200

    add iframe, material

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { Input, Button, Drawer } from '@material-ui/core';
+import Iframe from 'react-iframe'
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [isOpen, setIframeOpen] = useState(false)
+  const [url, setUrl] = useState("https://www.google.com/webhp?igu=1");
+
+  const handleOnBlur = (event) => {
+    setUrl(event.target.value)
+ }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Input className="input" name="Url" value={ url } onChange={handleOnBlur} label="Url to be rendered"></Input>
+        <Button color="primary" onClick={() => setIframeOpen(true)}>Render</Button>
+        <Drawer anchor="bottom" style={ { height: 'calc(100% - 64px)', top: 64 } } className="drawer" open={ isOpen }>   
+          <Button color="secondary" className="close-btn" onClick={ () => setIframeOpen(false) }>Close</Button>
+          <Iframe url={url}
+            width="auto"  
+            height="auto"
+            id="myId"
+            className="iframe"
+            display="initial"
+            position="relative"/>
+        </Drawer>
+        </header>
     </div>
   );
 }
